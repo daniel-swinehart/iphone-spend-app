6a89a2a7fe7f6331aa50f6d8922741f32c456a6e
daniel-swinehart <d4570s@gmail.com>
2020-12-10 21:38:06 +0100

    updated username

--- sqlconnect.php
+++ sqlconnect.php
@@ -1,6 +1,6 @@
 
 <?php
-   $mysqli = new mysqli("danielmaroc.com", "daniel", "8DcyhJ!&;0MY", "expense_tracker");
+   $mysqli = new mysqli("danielmaroc.com", "danielma_daniel", "8DcyhJ!&;0MY", "expense_tracker");
    if ($mysqli->connect_errno) {
       echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 }
